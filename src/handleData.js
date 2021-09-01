504c6edd55fb84638a0c3487eee19e90415bc8bb
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-01 01:42:47 -0500

    Simulate the data got by LocalStorage

---
+++ src/handleData.js
@@ -0,0 +1,32 @@
+const all_scores = [
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+    {
+        "Name": "Dante Alonso",
+        "Score": 25
+    },
+];
+
+export { all_scores };
