f9d8503bcd89e4aa4fd0338ac2f0ade81dd24e1c
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-02 21:40:56 -0500

    Create JS file to request data using POST and GET to the API

---
+++ src/handleAPI.js
@@ -0,0 +1,33 @@
+const urlGame = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/drTzlikoJDante/scores/';
+
+const postByAPI = async () => {
+  const nameInput = document.getElementById('floatingInput');
+  const scoreInput = document.getElementById('floatingScore');
+
+  const data = await fetch(urlGame, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(
+      {
+        user: `${nameInput.value}`,
+        score: `${scoreInput.value}`
+      },
+    ),
+  });
+  const res = await data.json();
+  return res;
+};
+
+const getByAPI = async (url) => {
+  const response = await fetch(url);
+
+  const result = await response.json();
+  if (result) {
+    document.getElementById('form-scores').reset;
+  }
+  return result;
+};
+
+export { postByAPI, getByAPI };
